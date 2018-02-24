e1954b443133a445ddeaa53e209389d1df7d0f39
noverb01@hotmail.com <noverb01@hotmail.com>
2018-02-23 20:52:30 -0600

    Add project files.

---
+++ ImHereMom-Client/BusinessLayer/MyFirebaseIidService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.Util;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using Firebase.Iid;
+
+namespace ImHereMom_Client.BusinessLayer
+{
+    [Service]
+    [IntentFilter (new[] {"com.google.firebase.INSTANCE_ID_EVENT"})]
+    public class MyFirebaseIidService: FirebaseInstanceIdService
+    {
+        const string TAG = "MyFirebaseIidService";
+
+        public override void OnTokenRefresh()
+        {
+            //base.OnTokenRefresh();
+            var refreshedToken = FirebaseInstanceId.Instance.Token;
+            Log.Debug(TAG, $"Refreshed Token: {refreshedToken}");
+            SendRegistrationToServer(refreshedToken);
+        }
+
+        private void SendRegistrationToServer(string token)
+        {
+            Log.Debug(TAG, $"If you had a Server, this token {token} would be sent to it");
+            // TBD what to do
+        }
+    }
+}
---
+++ ImHereMom-Client/MainActivity.cs
@@ -0,0 +1,85 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Gms.Common;
+using Android.Util;
+using Firebase.Messaging;
+using Firebase.Iid;
+using ImHereMom_Client.BusinessLayer;
+using Firebase;
+
+namespace ImHereMom_Client
+{
+    [Activity(Label = "I'm Here Mom", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        const string TAG = "MainActivity";
+        TextView txtMsg;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            txtMsg = FindViewById<TextView>(Resource.Id.txtMsg);
+            Button btnlogToken = FindViewById<Button>(Resource.Id.btnlogToken);
+
+            IsPlayServicesAvailable();
+            btnlogToken.Click += delegate
+            {
+                try
+                {
+                    Log.Debug(TAG, $"Instance Id Token: {FirebaseInstanceId.Instance.Token}");
+                    Toast.MakeText(this, $"Instance Id Token: {FirebaseInstanceId.Instance.Token}", ToastLength.Long).Show();
+                }
+                catch (System.Exception e)
+                {
+                    // When requesting the token, check for connectivity to the internet
+                    Toast.MakeText(this, $"Check your Internet connection. {e.Message}", ToastLength.Long).Show();
+                }
+            };
+            
+            if (Intent.Extras != null)
+            {
+                foreach (var key in Intent.Extras.KeySet())
+                {
+                    var value = Intent.Extras.GetString(key);
+                    Log.Debug(TAG, $"Key: {key} - Value: {value}");
+                    Toast.MakeText(this, $"Key: {key} - Value: {value}",ToastLength.Long);
+                }
+            }
+
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            IsPlayServicesAvailable();
+        }
+
+        public bool IsPlayServicesAvailable()
+        {
+            // There is a result code of type integer
+            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+
+            if (resultCode != ConnectionResult.Success)
+            {
+                if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
+                {
+                    txtMsg.Text = GoogleApiAvailability.Instance.GetErrorString(resultCode);
+                }
+                else
+                {
+                    txtMsg.Text = "Sorry pal, this device is not supported. You need a newer version of Google Play Services";
+                    Finish(); // Close the Activity!!!
+                }
+                return false;
+            }
+            else
+            {
+                txtMsg.Text = "You are in luck! Google Play Services is up-to-par";
+                return true;
+            }
+        }
+    }
+}
+
